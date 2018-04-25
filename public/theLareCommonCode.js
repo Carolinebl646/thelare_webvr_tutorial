a08d9cb13c2c7a329469866fdd1413e4a192d4c7
Carolinebl646 <38509988+Carolinebl646@users.noreply.github.com>
2018-04-25 17:59:32 -0400

    Update theLareCommonCode.js

--- public/theLareCommonCode.js
+++ public/theLareCommonCode.js
@@ -16,7 +16,7 @@ var setReadyEvents = function(event) {
 
   drawingRoom.addHotspot('spot1', {
     pitch: 0,
-    yaw: 90,
+    yaw: -90,
     radius: 0.10,
     distance: 1
   });
@@ -30,7 +30,7 @@ var setReadyEvents = function(event) {
 
   drawingRoom.addHotspot('spot3', {
     pitch: 50,
-    yaw: 90,
+    yaw: 180,
     radius: 0.10,
     distance: 1
   });
